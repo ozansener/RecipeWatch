98b6f88c7ed8caa6814724934079e74848570822
Ozan Sener <os79@brane02.cs.cornell.edu>
2015-01-08 20:57:31 -0500

    a

--- src/RecipeVision/coCluster.py
+++ src/RecipeVision/coCluster.py
@@ -2,17 +2,40 @@ from sklearn.cluster import AffinityPropagation
 from sklearn import metrics
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import NearestNeighbors
+from sklearn.cluster import SpectralClustering
+
 
 from ModalDB import *
 import numpy as np
 import scipy as sp
+import pickle
+import glob
 
 class CoCluster:
   def FetchDataFromModalDB(self,ModalClient,featureName='CNNFeatures'):
     """
     Iterate over entire ModalDB, gets all the features. The features should be pre-computed
     """
-    self.data_points = np.loadtxt("mnist2500_X.txt");
+
+    A = glob.glob('=*.bn')
+    print A
+    idxs = []
+    feats = []
+    for fil in A:
+      dat = pickle.load(open(fil,'rb'))
+      for vids in dat:
+        for feat in vids['Features']:
+          idxs.append(vids['name']+'_'+str(feat))
+          feats.append(vids['Features'][feat])
+      if not ModalClient == 42:
+        break
+    FeatArray = np.array(feats)
+    XX = FeatArray.astype('float64')
+
+    pickle.dump(idxs,open('idxs.bn','wb'))
+
+    self.data_points = XX;
     self.labels_gt = np.loadtxt("mnist2500_labels.txt");
     return 0
 
@@ -26,14 +49,18 @@ class CoCluster:
     """
     Run the  affinity propogation algorithm. It is used as a baseline in the paper
     """
-    af = AffinityPropagation(preference=-600).fit(self.data_points)
+    af = AffinityPropagation(preference=-2000000).fit(self.data_points)
     self.labels_ap = af.labels_
+    pickle.dump(af.labels_,open('affinity_prop.bn','wb'))
     return 0
 
   def runSpectralClustering(self):
     """
     Run the basic normalized cut algorithm
     """
+    af = SpectralClustering(n_clusters=25,n_neighbors=10, assign_labels='discretize').fit(self.data_points)
+    self.labels_ap = af.labels_
+    pickle.dump(af.labels_,open('spectral_prop.bn','wb'))
     return 0
 
   def runCoClustering(self):
