fedfc1abebaf6ec0b40668337cb91df3fcd1c00f
Ozan Sener <ozansener@gmail.com>
2015-01-10 17:52:59 -0800

    Spectral

--- src/RecipeVision/coCluster.py
+++ src/RecipeVision/coCluster.py
@@ -10,6 +10,7 @@ from ModalDB import *
 import numpy as np
 import scipy as sp
 import scipy.sparse
+import scipy.sparse.linalg as salg
 
 import pickle
 import glob
@@ -19,6 +20,8 @@ class CoCluster:
     """
     Iterate over entire ModalDB, gets all the features. The features should be pre-computed
     """
+
+    dcnt = 0
 
     A = glob.glob('=*.bn')
     print A
@@ -30,6 +33,9 @@ class CoCluster:
         for feat in vids['Features']:
           idxs.append(vids['name']+'_'+str(feat))
           feats.append(vids['Features'][feat])
+        if dcnt>100:
+          break
+        dcnt = dcnt+1
       if not ModalClient == 42:
         break
     FeatArray = np.array(feats)
@@ -49,19 +55,20 @@ class CoCluster:
 
   def getClusters(self):
     idxs = np.array(range(0,self.PDistMat.shape[0]))
-    while len(self.clusters<100):
+    while len(self.clusters)<100:
       #First let's find the eigenvector corresponding to the largest eigen value
       vals, vecs = sp.sparse.linalg.eigsh(self.PDistMat, k=1,which='LM')
       #Now we will discreteize it to find the best threshold
       sortVals = np.sort(vecs,axis=0)
       tempVals = vecs[:,0].copy()
-      binVals = np.ones(temp.Vals.shape)
-      finVals = binVals.copy()
+      binVals = np.ones(tempVals.shape)
       curCost = 0
+      bestCost = 0
       for i in range(min(1000,sortVals.shape[0]-1),sortVals.shape[0]):
         curT =sortVals[i,0]
         binVals[tempVals<=curT]=0
-        curCost = (binVals.T*self.PDistNat*binVals)/(binVals.T*binVals)
+        bM = np.mat(binVals).T
+        curCost = (bM.T*self.PDistMat*bM)/(bM.T*bM)
         if curCost>bestCost:
           bestCost = curCost
           finVals = binVals.copy()
